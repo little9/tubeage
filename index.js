99b4b27fcc9fb1aef934271ebed6a0fbec564b3b
James Little <j.little@miami.edu>
2015-11-05 19:52:19 -0500

    Adding quality settings

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 #!/usr/bin/env node
 
-var Tubage = {
-    settings: {
+var Tubeage = {
+
+    strings : {
         thumbnailUrl: "http://img.youtube.com/vi/YOUTUBEID/THUMBQUALITY",
-        standardQuality: "sddefault.jpg",
         youTubeUrl: "https://www.youtube.com/watch?v=YOUTUBEID",
         markUp: '<a href="YOUTUBEURL"><img src="THUMBURL"/></a>',
+        noEmbedCodeError : "You should provide a YouTube embed code"
     },
-    outputEmbedMarkup: function (youTubeId) {
-        var ytUrl = Tubage.settings.youTubeUrl.replace("YOUTUBEID", youTubeId);
-        var thumbUrl = Tubage.settings.thumbnailUrl
-                       .replace("YOUTUBEID", youTubeId)
-                       .replace("THUMBQUALITY", Tubage.settings.standardQuality);
-        var markUp = Tubage.settings.markUp.replace('YOUTUBEURL', ytUrl)
+    youTubeQuality : {
+        default : "default.jpg",
+        sd : "sddefault.jpg",
+        hq : "hqdefault.jpg",
+        mq : "mqdefault.jpg",
+        max : "maxresdefault.jpg"
+    },
+    userArgs : {
+        embedCode : 'undefined',
+        quality : 'undefined'
+    },
+    outputEmbedMarkup: function (args) {
+       
+        var ytUrl = Tubeage.strings.youTubeUrl.replace("YOUTUBEID", args.embedCode);
+        var thumbUrl = Tubeage.strings.thumbnailUrl
+                       .replace("YOUTUBEID", args.embedCode)
+                       .replace("THUMBQUALITY", args.quality);
+        var markUp = Tubeage.strings.markUp.replace('YOUTUBEURL', ytUrl)
                                            .replace('THUMBURL', thumbUrl);
 
-        
         return markUp;
     },
     processArgs: function (args) {  
-        var idArg = args[2];
-        return idArg;
-        console.log(args);
+        var myArgs = Tubeage.userArgs;
+
+        if (typeof args[2] === 'undefined'){ 
+            console.log(Tubeage.strings.noEmbedCodeError)
+        } else {
+            myArgs.embedCode = args[2];
+        }
+
+        if (typeof args[3] === 'undefined'){ 
+            myArgs.quality = Tubeage.youTubeQuality.default;
+        } else {
+            myArgs.quality = Tubeage.youTubeQuality[args[3]];
+        }
+        
+        return myArgs;
+        
     }
 };
 
-console.log(Tubage.outputEmbedMarkup(Tubage.processArgs(process.argv)));
-module.exports.Tubage = Tubage;
+console.log(Tubeage.outputEmbedMarkup(Tubeage.processArgs(process.argv)));
+module.exports.Tubeage = Tubeage;
--- test/test.js
+++ test/test.js
@@ -1,20 +1,83 @@
 var assert = require('assert'),
-    tubage = require('./../index').Tubage;
+    tubeage = require('./../index').Tubeage;
 describe('Tubeage', function () {
     describe('#outputEmbedMarkup', function () {
-        it('should return html markup based on the YouTube ID it is given', function () {
-            var markup = tubage.outputEmbedMarkup("bGhSHXebjbE");
-            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/sddefault.jpg"/></a>';
+        it('should return html markup based on the YouTube ID it is given with default quality', function () {
+
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'default.jpg'
+            }
+            var markup = tubeage.outputEmbedMarkup(userArgs);
+            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/default.jpg"/></a>';
             assert.equal(markup, requiredMarkup);
         });
     });
 });
+
 describe('Tubeage', function () {
     describe('#processArgs', function () {
-        it('it should process the command line arguments and get the first parameter', function () {
-            var args = ['node', 'index.js', 'bGhSHXebjbE', 'foo'];
-            var ytId = tubage.processArgs(args);
-            assert.equal(ytId,'bGhSHXebjbE')
+        it('it should process the command line arguments return an object with the embed code and quality', function () {
+            var args = ['node', 'index.js', 'bGhSHXebjbE'];
+            var myArgs = tubeage.processArgs(args);
+
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'default.jpg'
+            }
+            assert.equal(myArgs.embedCode, userArgs.embedCode)
+            assert.equal(myArgs.quality, userArgs.quality)
+
+        });
+
+
+
+    });
+});
+
+describe('Tubeage', function () {
+    describe('#outputEmbedMarkup', function () {
+        it('should return html markup based on the YouTube ID it is given and allow you to select high quality', function () {
+
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'hqdefault.jpg'
+            }
+
+            var markup = tubeage.outputEmbedMarkup(userArgs);
+            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/hqdefault.jpg"/></a>';
+            assert.equal(markup, requiredMarkup);
+        });
+
+        it('... and allow you to select medium quality', function () {
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'mqdefault.jpg'
+            }
+
+            var markup = tubeage.outputEmbedMarkup(userArgs);
+            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/mqdefault.jpg"/></a>';
+            assert.equal(markup, requiredMarkup);
+        });
+        it('... and standard quality', function () {
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'sddefault.jpg'
+            }
+
+            var markup = tubeage.outputEmbedMarkup(userArgs);
+            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/sddefault.jpg"/></a>';
+            assert.equal(markup, requiredMarkup);
+        });
+        it('... and maximum quality', function () {
+            var userArgs = {
+                embedCode: 'bGhSHXebjbE',
+                quality: 'maxresdefault.jpg'
+            }
+
+            var markup = tubeage.outputEmbedMarkup(userArgs);
+            var requiredMarkup = '<a href="https://www.youtube.com/watch?v=bGhSHXebjbE"><img src="http://img.youtube.com/vi/bGhSHXebjbE/maxresdefault.jpg"/></a>';
+            assert.equal(markup, requiredMarkup);
         });
     });
 });
